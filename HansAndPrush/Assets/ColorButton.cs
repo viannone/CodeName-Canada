fca8d44e9e08f965bb189c7297ce119cccb9bb6a
viannone <VIannone@Vincents-iMac.local>
2017-02-01 20:00:53 -0800

    Try again

--- HansAndPrush/Assets/ColorButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-
-public class ColorButton : MonoBehaviour {
-	public Button button;
-	public bool isInRevolver = false;
-	public bool isUnlocked = true;
-}
